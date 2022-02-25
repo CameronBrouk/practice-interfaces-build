3bdb4ed0f21f664c19b3829c21dd4eebbdcccc59
Cameron Brouk <cameronbrouk@gmail.com>
2022-02-25 15:40:30 -0600

    husky test

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,8 @@
 import { InferArrayElement } from "./utils";
 console.log("test");
+console.log("test");
+console.log("test");
+console.log("test");
 
 const add = (n1: number) => (n2: number) => n1 + n2;
 
